b73cb5819dde2669647f59b24b452445940ab9ce
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-11-25 22:49:35 +0600

    card information set done

--- src/components/availablePlayers/availablePlayers.jsx
+++ src/components/availablePlayers/availablePlayers.jsx
@@ -4,7 +4,6 @@ import flag from "../../assets/report 1.png";
 
 const AvailablePlayers = ({ playersPromise }) => {
   const playerData = use(playersPromise);
-console.log(playerData)
   //   const mountedRef = useRef(false);
 
   //   useEffect(() => {
@@ -16,53 +15,54 @@ console.log(playerData)
   //     console.log(playerData);
   //   }, [playerData]);
   return (
-    <div className="w-[90%] mx-auto">
+    <div className="w-[90%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {playerData.map((player) => (
+        <div
+          key={player.id}
+          className="card bg-base-100  shadow-sm p-[1rem] bg-white space-y-4"
+        >
+          <figure className="mb-[1rem]">
+            <img
+              src={player.img}
+              alt={`${player.name} image`}
+              className="object-cover h-[200px] w-full rounded-lg"
+            />
+          </figure>
+          <div className="bg-white mb-[1rem] ">
+            <h2 className="card-title ml-2">
+              {" "}
+              <img src={userImg} alt="user image" /> {player.name}
+            </h2>
+          </div>
+          <div className="bg-white  ml-2 flex justify-between mr-2">
+            <p className="flex items-center gap-2 opacity-[0.5]">
+              <img src={flag} alt="flag" /> {player.country}
+            </p>
+            <button className="p-2 rounded bg-[#1313130D]">
+              {player.role}
+            </button>
+          </div>
+          <p className="ml-2">
+            <b>Rating</b>
+            {` ${player.rating}`}
+          </p>
+          <div className="bg-white mb-[1rem] ml-2 flex justify-between mr-2">
+            <p>
+              <b>{player.battingStyle}</b>
+            </p>
+            <p className="text-[#131313b3]">{player.bowlingStyle}</p>
+          </div>
 
-    {playerData.map(player => (
-            <div key={player.id} className="card bg-base-100 w-96 shadow-sm p-[1rem] bg-white space-y-4">
-              <figure className="mb-[1rem]">
-                <img
-                  src={player.img}
-                  alt={`${player.name} image`}
-                  className="object-cover h-[200px] w-full rounded-lg"
-                />
-              </figure>
-              <div className="bg-white mb-[1rem] ">
-                <h2 className="card-title ml-2">
-                  {" "}
-                  <img src={userImg} alt="user image" /> {player.name}
-                </h2>
-              </div>
-              <div className="bg-white  ml-2 flex justify-between mr-2">
-                <p className="flex items-center gap-2 opacity-[0.5]">
-                  <img src={flag} alt="flag" /> India
-                </p>
-                <button className="p-2 rounded bg-[#1313130D]">
-                  All-Rounder
-                </button>
-              </div>
-              <p className="ml-2">
-                <b>Rating</b>
-              </p>
-              <div className="bg-white mb-[1rem] ml-2 flex justify-between mr-2">
-                <p>
-                  <b>Left-Hand-Bat</b>
-                </p>
-                <p className="text-[#131313b3]">Left-Hand-Bat</p>
-              </div>
-
-              <div className="card-actions justify-between items-center ml-2 mr-2 mb-1">
-                <p>
-                  <b>Price:$ 1500000</b>
-                </p>
-                <button className="border border-box border-[#1313131a] p-2 rounded-lg">
-                  Choose Player
-                </button>
-              </div>
-            </div>
-        ))
-    }
-      
+          <div className="card-actions justify-between items-center ml-2 mr-2 mb-1">
+            <p>
+              <b>Price:$ {player.price}</b>
+            </p>
+            <button className="border border-box border-[#1313131a] p-2 rounded-lg">
+              Choose Player
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
